35e9b22d8358eb695ea20a57d76f8b9079670beb
Szymon Duda <91761037+MisterChamster@users.noreply.github.com>
2026-04-08 17:05:25 +0200

    Instrukcje :-)

---
+++ src/components/mcts.h
@@ -0,0 +1,18 @@
+// Struktura drzewa gry (historia)
+// Coś jak w CERCI
+// Przeciwnik jest biały :-(
+// Zaimplementować zeby od pcozatku grac z kompem
+
+// AI ma planszę, patrzy na moliwe ruchy i wybiera kt
+// ory ruch zagrac. Funkcja oceniająca (kazdy) ruch 
+// Jak dobry jest. Moze byc w nowym pliku. Na początku
+// zwraca 1 (wywalone balls). 
+// Wszystkie wyniki (priori) zostają zapisane i wrzucone 
+// do softmaxa. Slay. Wynik wrzucamy do funkcji 
+// ucb i powtarzam do momentu az dojde do liścia. W tym punkcie
+// zapisuję stan to ktorego chce wrocic i wykonuje 29172478014
+// losowych gier (albo priori+confidence band),
+// szukając najlepszej ez. Zliczam wygrane i przegrane.
+// 
+
+
